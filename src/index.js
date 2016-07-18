bff5b7e14a825cd9cad5b59ef4e1a62e9943f17a
deegles <diegoben@amazon.com>
2016-07-18 16:50:30 -0700

    Initial commit of HowTo skill sample.

---
+++ src/index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined; //OPTIONAL: replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+var SKILL_NAME = 'Minecraft Helper';
+var recipes = require('./recipes');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'NewSession': function () {
+        this.attributes['speechOutput'] = 'Welcome to ' + SKILL_NAME + '. You can ask a question like, what\'s the' +
+            ' recipe for a chest? ... Now, what can I help you with.';
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes['repromptSpeech'] = 'For instructions on what you can say, please say help me.';
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'RecipeIntent': function () {
+        var itemSlot = this.event.request.intent.slots.Item;
+        var itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        var cardTitle = SKILL_NAME + ' - Recipe for ' + itemName;
+        var recipe = recipes[itemName];
+
+        if (recipe) {
+            this.attributes['speechOutput'] = recipe;
+            this.attributes['repromptSpeech'] = 'Try saying repeat.';
+            this.emit(':askWithCard', recipe, this.attributes['repromptSpeech'], cardTitle, recipe);
+        } else {
+            var speechOutput = 'I\'m sorry, I currently do not know ';
+            var repromptSpeech = 'What else can I help with?';
+            if (itemName) {
+                speechOutput = 'the recipe for ' + itemName + '. ';
+            } else {
+                speechOutput = 'that recipe. ';
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes['speechOutput'] = speechOutput;
+            this.attributes['repromptSpeech'] = repromptSpeech;
+
+            this.emit(':ask', speechOutput, repromptSpeech);
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes['speechOutput'] = 'You can ask questions such as, what\'s the recipe, or, you can say exit... ' +
+            'Now, what can I help you with?';
+        this.attributes['repromptSpeech'] = 'You can say things like, what\'s the recipe, or you can say exit...' +
+            ' Now, what can I help you with?';
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest':function () {
+        this.emit(':tell', 'Goodbye!');
+    }
+};
