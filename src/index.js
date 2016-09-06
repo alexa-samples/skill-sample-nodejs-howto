e0118814fdc0148ce18ad81fb18e6a797cb4821d
Ludovic Roguet <ludovic.roguet@nordcloud.se>
2016-09-06 21:48:07 +0200

    The property on the LambdaHandler is appId and not APP_ID.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ var recipes = require('./recipes');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
+    alexa.appId = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
