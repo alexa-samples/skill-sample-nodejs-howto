31ea936657045855587917252fcd5a523058cacd
ygzy <ygzg@amazon.com>
2016-09-06 13:54:16 -0700

    How-to skill i18n initail commit

---
+++ src/index.js
@@ -0,0 +1,110 @@
+'use strict';
+
+var Alexa = require('alexa-sdk');
+var APP_ID = undefined; //OPTIONAL: replace with 'amzn1.echo-sdk_inter-ams.app.[your-unique-value-here]';
+var recipes = require('./recipes');
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    //add resource to enable i18n, otherwise i18n module functions will not be loaded
+    alexa.resources = languageStrings;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
+
+var handlers = {
+    'NewSession': function () {
+        this.attributes['speechOutput'] = this.t("WELCOME_MESSAGE", this.t("SKILL_NAME"));
+        // If the user either does not reply to the welcome message or says something that is not
+        // understood, they will be prompted again with this text.
+        this.attributes['repromptSpeech'] = this.t("WELCOME_REPROMT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'RecipeIntent': function () {
+        var itemSlot = this.event.request.intent.slots.Item;
+        var itemName;
+        if (itemSlot && itemSlot.value) {
+            itemName = itemSlot.value.toLowerCase();
+        }
+
+        var cardTitle = this.t("DISPLAY_CARD_TITLE", this.t("SKILL_NAME"), itemName);
+        var recipes = this.t("RECIPES");
+        var recipe = recipes[itemName];
+        console.log(recipe);
+
+        if (recipe) {
+            this.attributes['speechOutput'] = recipe;
+            this.attributes['repromptSpeech'] = this.t("RECIPE_REPEAT_MESSAGE");
+            this.emit(':askWithCard', recipe, this.attributes['repromptSpeech'], cardTitle, recipe);
+        } else {
+            var speechOutput = this.t("RECIPE_NOT_FOUND_MESSAGE");
+            var repromptSpeech = this.t("RECIPE_NOT_FOUND_REPROMPT");
+            if (itemName) {
+                speechOutput += this.t("RECIPE_NOT_FOUND_WITH_ITEM_NAME", itemName);
+            } else {
+                speechOutput += this.t("RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME");
+            }
+            speechOutput += repromptSpeech;
+
+            this.attributes['speechOutput'] = speechOutput;
+            this.attributes['repromptSpeech'] = repromptSpeech;
+
+            this.emit(':ask', speechOutput, repromptSpeech);
+        }
+    },
+    'AMAZON.HelpIntent': function () {
+        this.attributes['speechOutput'] = this.t("HELP_MESSAGE");
+        this.attributes['repromptSpeech'] = this.t("HELP_REPROMT");
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.RepeatIntent': function () {
+        this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptSpeech'])
+    },
+    'AMAZON.StopIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'AMAZON.CancelIntent': function () {
+        this.emit('SessionEndedRequest');
+    },
+    'SessionEndedRequest':function () {
+        this.emit(':tell', this.t("STOP_MESSAGE"));
+    }
+};
+
+var languageStrings = {
+    "en-GB": {
+        "translation": {
+            "RECIPES": recipes.RECIPE_EN_GB,
+            "SKILL_NAME": "British Minecraft Helper",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
+            "WELCOME_REPROMT": "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
+            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s.",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+        }
+    },
+    "en-US": {
+        "translation": {
+            "RECIPES" : recipes.RECIPE_EN_US,
+            "SKILL_NAME" : "American Minecraft Helper",
+            "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
+            "WELCOME_REPROMT": "For instructions on what you can say, please say help me.",
+            "DISPLAY_CARD_TITLE": "s%  - Recipe for %s.",
+            "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
+            "HELP_REPROMT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
+            "STOP_MESSAGE": "Goodbye!",
+            "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
+            "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s.",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+        }
+    }
+};
