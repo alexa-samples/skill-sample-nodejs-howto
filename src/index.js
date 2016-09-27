9c902374522e50404acce57e86f8dd28b89a2e20
ygzy <ygzg@amazon.com>
2016-09-27 10:16:12 -0700

    Add German Strings

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ var recipes = require('./recipes');
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
+    //add resource to enable i18n, otherwise i18n module functions will not be loaded
     alexa.resources = languageStrings;
     alexa.registerHandlers(handlers);
     alexa.execute();
@@ -84,7 +84,7 @@ var languageStrings = {
             "STOP_MESSAGE": "Goodbye!",
             "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
             "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s.",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
             "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
             "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
         }
@@ -95,15 +95,32 @@ var languageStrings = {
             "SKILL_NAME" : "American Minecraft Helper",
             "WELCOME_MESSAGE": "Welcome to %s. You can ask a question like, what\'s the recipe for a chest? ... Now, what can I help you with.",
             "WELCOME_REPROMT": "For instructions on what you can say, please say help me.",
-            "DISPLAY_CARD_TITLE": "s%  - Recipe for %s.",
+            "DISPLAY_CARD_TITLE": "%s  - Recipe for %s.",
             "HELP_MESSAGE": "You can ask questions such as, what\'s the recipe, or, you can say exit...Now, what can I help you with?",
             "HELP_REPROMT": "You can say things like, what\'s the recipe, or you can say exit...Now, what can I help you with?",
             "STOP_MESSAGE": "Goodbye!",
             "RECIPE_REPEAT_MESSAGE": "Try saying repeat.",
             "RECIPE_NOT_FOUND_MESSAGE": "I\'m sorry, I currently do not know ",
-            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s.",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "the recipe for %s. ",
             "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "that recipe. ",
             "RECIPE_NOT_FOUND_REPROMPT": "What else can I help with?"
+        }
+    },
+    "de-DE": {
+        "translation": {
+            "RECIPES" : recipes.RECIPE_DE_DE,
+            "SKILL_NAME" : "Assistent für Minecraft in Deutsch",
+            "WELCOME_MESSAGE": "Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine Truhe? ... Nun, womit kann ich dir helfen?",
+            "WELCOME_REPROMT": "Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.",
+            "DISPLAY_CARD_TITLE": "%s - Rezept für %s.",
+            "HELP_MESSAGE": "Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
+            "HELP_REPROMT": "Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?",
+            "STOP_MESSAGE": "Auf Wiedersehen!",
+            "RECIPE_REPEAT_MESSAGE": "Sage einfach „Wiederholen“.",
+            "RECIPE_NOT_FOUND_MESSAGE": "Tut mir leid, ich kenne derzeit ",
+            "RECIPE_NOT_FOUND_WITH_ITEM_NAME": "das Rezept für %s nicht. ",
+            "RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME": "dieses Rezept nicht. ",
+            "RECIPE_NOT_FOUND_REPROMPT": "Womit kann ich dir sonst helfen?"
         }
     }
 };
