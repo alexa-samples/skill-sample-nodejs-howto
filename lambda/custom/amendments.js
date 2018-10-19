42fd7f3f93e760f962608f762df435625370baf9
Ker Lee Yap <kerlee_yap@intuit.com>
2018-10-19 13:38:22 -0700

    first attempt

---
+++ lambda/custom/amendments.js
@@ -0,0 +1,18 @@
+/* eslint-disable  func-names */
+/* eslint-disable max-len */
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+  'AMENDMENT_EN_US': {
+    'first': 'Freedom of speech, religion, chill out in a group, and complain about the government.',
+    'second': 'Right to bear arms. Pew pew.',
+    'third': 'Quartering soldiers. The irrelevant one about how soldiers can\'t squat at your place',
+    'fourth': 'Search and seizure. People can\'t take your stuff',
+    'fifth': 'Right of persons. Right to remain silent.',
+    'sixth': 'Speedy and public trial',
+    'seventh': 'Right to have a jury if you get sued for something worth more than twenty bucks',
+    'eighth': 'Bail and fines have to be kind of cheap and no funky punishments',
+    'ninth': 'You have more rights than the ones in the constitution.',
+    'tenth': 'Powers not listed in the constitution either belong to the states or you.',
+    'eleventh': 'Suits Against States'
+    }
+};
--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable  no-console */
 
 const Alexa = require('ask-sdk-core');
-const recipes = require('./recipes');
+const amendments = require('./amendments');
 const i18n = require('i18next');
 const sprintf = require('i18next-sprintf-postprocessor');
 
@@ -15,7 +15,7 @@ const LaunchRequestHandler = {
     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
-    const item = requestAttributes.t(getRandomItem(Object.keys(recipes.RECIPE_EN_US)));
+    const item = requestAttributes.t(getRandomItem(Object.keys(amendments.AMENDMENT_EN_US)));
 
     const speakOutput = requestAttributes.t('WELCOME_MESSAGE', requestAttributes.t('SKILL_NAME'), item);
     const repromptOutput = requestAttributes.t('WELCOME_REPROMPT');
@@ -45,28 +45,28 @@ const RecipeHandler = {
     }
 
     const cardTitle = requestAttributes.t('DISPLAY_CARD_TITLE', requestAttributes.t('SKILL_NAME'), itemName);
-    const myRecipes = requestAttributes.t('RECIPES');
-    const recipe = myRecipes[itemName];
+    const amendments = requestAttributes.t('AMENDMENTS');
+    const amendment = amendments[itemName];
     let speakOutput = "";
 
-    if (recipe) {
-      sessionAttributes.speakOutput = recipe;
-      //sessionAttributes.repromptSpeech = requestAttributes.t('RECIPE_REPEAT_MESSAGE');
+    if (amendment) {
+      sessionAttributes.speakOutput = amendment;
+      //sessionAttributes.repromptSpeech = requestAttributes.t('AMENDMENT_REPEAT_MESSAGE');
       handlerInput.attributesManager.setSessionAttributes(sessionAttributes);
 
       return handlerInput.responseBuilder
         .speak(sessionAttributes.speakOutput) // .reprompt(sessionAttributes.repromptSpeech)
-        .withSimpleCard(cardTitle, recipe)
+        .withSimpleCard(cardTitle, amendment)
         .getResponse();
     }
     else{
-      speakOutput = requestAttributes.t('RECIPE_NOT_FOUND_MESSAGE');
-      const repromptSpeech = requestAttributes.t('RECIPE_NOT_FOUND_REPROMPT');
+      const repromptSpeech = requestAttributes.t('AMENDMENT_NOT_FOUND_REPROMPT');
       if (itemName) {
-        speakOutput += requestAttributes.t('RECIPE_NOT_FOUND_WITH_ITEM_NAME', itemName);
+        speakOutput += requestAttributes.t('AMENDMENT_NOT_FOUND_WITH_ITEM_NAME', itemName);
       } else {
-        speakOutput += requestAttributes.t('RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME');
+        speakOutput += requestAttributes.t('AMENDMENT_NOT_FOUND_WITHOUT_ITEM_NAME');
       }
+      speakOutput += requestAttributes.t('AMENDMENT_NOT_FOUND_MESSAGE');
       speakOutput += repromptSpeech;
 
       sessionAttributes.speakOutput = speakOutput; //saving speakOutput to attributes, so we can use it to repeat
@@ -91,7 +91,7 @@ const HelpHandler = {
     const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
     const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
 
-    const item = requestAttributes.t(getRandomItem(Object.keys(recipes.RECIPE_EN_US)));
+    const item = requestAttributes.t(getRandomItem(Object.keys(amendments.AMENDMENT_EN_US)));
 
     sessionAttributes.speakOutput = requestAttributes.t('HELP_MESSAGE', item);
     sessionAttributes.repromptSpeech = requestAttributes.t('HELP_REPROMPT', item);
@@ -165,50 +165,27 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 const languageStrings = {
   en: {
     translation: {
-      RECIPES: recipes.RECIPE_EN_US,
-      SKILL_NAME: 'Minecraft Helper',
-      WELCOME_MESSAGE: 'Welcome to %s. You can ask a question like, what\'s the recipe for a %s? ... Now, what can I help you with?',
+      AMENDMENTS: amendments.AMENDMENT_EN_US,
+      SKILL_NAME: 'US Constitution TLDR',
+      WELCOME_MESSAGE: 'Welcome to %s. You can ask a question like, what\'s the %s amendment? ... Now, what can I help you with?',
       WELCOME_REPROMPT: 'For instructions on what you can say, please say help me.',
       DISPLAY_CARD_TITLE: '%s  - Recipe for %s.',
-      HELP_MESSAGE: 'You can ask questions such as, what\'s the recipe for a %s, or, you can say exit...Now, what can I help you with?',
-      HELP_REPROMPT: 'You can say things like, what\'s the recipe for a %s, or you can say exit...Now, what can I help you with?',
+      HELP_MESSAGE: 'You can ask questions such as, what\'s the %s amendment, or, you can say exit...Now, what can I help you with?',
+      HELP_REPROMPT: 'Hint: there are only twenty three amendments. You can say things like, what\'s the %s amendment, or you can say exit...Now, what can I help you with?',
       STOP_MESSAGE: 'Goodbye!',
-      RECIPE_REPEAT_MESSAGE: 'Try saying repeat.',
-      RECIPE_NOT_FOUND_MESSAGE: 'I\'m sorry, I currently do not know ',
-      RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'the recipe for %s. ',
-      RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'that recipe. ',
-      RECIPE_NOT_FOUND_REPROMPT: 'What else can I help with?'
+      AMENDMENT_REPEAT_MESSAGE: 'Try saying repeat.',
+      AMENDMENT_NOT_FOUND_MESSAGE: ' doesn\'t exist',
+      AMENDMENT_NOT_FOUND_WITH_ITEM_NAME: 'The %s amendment',
+      AMENDMENT_NOT_FOUND_WITHOUT_ITEM_NAME: 'That amendment',
+      AMENDMENT_NOT_FOUND_REPROMPT: 'What else can I help with?'
     },
   },
   'en-US': {
     translation: {
-      RECIPES: recipes.RECIPE_EN_US,
-      SKILL_NAME: 'American Minecraft Helper'
+      AMENDMENTS: amendments.AMENDMENT_EN_US,
+      SKILL_NAME: 'US Constitution TLDR'
     },
-  },
-  'en-GB': {
-    translation: {
-      RECIPES: recipes.RECIPE_EN_GB,
-      SKILL_NAME: 'British Minecraft Helper'
-    },
-  },
-  de: {
-    translation: {
-      RECIPES: recipes.RECIPE_DE_DE,
-      SKILL_NAME: 'Assistent für Minecraft in Deutsch',
-      WELCOME_MESSAGE: 'Willkommen bei %s. Du kannst beispielsweise die Frage stellen: Welche Rezepte gibt es für eine %s? ... Nun, womit kann ich dir helfen?',
-      WELCOME_REPROMPT: 'Wenn du wissen möchtest, was du sagen kannst, sag einfach „Hilf mir“.',
-      DISPLAY_CARD_TITLE: '%s - Rezept für %s.',
-      HELP_MESSAGE: 'Du kannst beispielsweise Fragen stellen wie „Wie geht das Rezept für eine %s“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-      HELP_REPROMPT: 'Du kannst beispielsweise Sachen sagen wie „Wie geht das Rezept für eine %s“ oder du kannst „Beenden“ sagen ... Wie kann ich dir helfen?',
-      STOP_MESSAGE: 'Auf Wiedersehen!',
-      RECIPE_REPEAT_MESSAGE: 'Sage einfach „Wiederholen“.',
-      RECIPE_NOT_FOUND_MESSAGE: 'Tut mir leid, ich kenne derzeit ',
-      RECIPE_NOT_FOUND_WITH_ITEM_NAME: 'das Rezept für %s nicht. ',
-      RECIPE_NOT_FOUND_WITHOUT_ITEM_NAME: 'dieses Rezept nicht. ',
-      RECIPE_NOT_FOUND_REPROMPT: 'Womit kann ich dir sonst helfen?'
-    },
-  },
+  }
 };
 
 // Finding the locale of the user
